00fb518ab3c9e87331e28275ae9c3bd634838873
ckshei <cks856@gmail.com>
2016-10-11 19:11:00 -0400

    cleaned up user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,6 @@ class User < ActiveRecord::Base
   serialize :top_tracks, Array
   serialize :top_artists, Array
 
-  has_secure_password validations: false
-
-  has_many :songs
-  # accepts_nested_attributes_for :songs, reject_if: proc { |attributes| attributes['name'].blank? }
   #for host
   has_many :events, :foreign_key => 'host_id'
   has_many :guests, through: :events 
@@ -14,23 +10,8 @@ class User < ActiveRecord::Base
   has_many :event_guests, :foreign_key => "guest_id"
   has_many :reservations, through: :event_guests, :source =>"event"
 
-  validates :password, presence:true, length: {minimum: 6}, unless: :has_uid
-  validates :display_name, presence:true
-  validates :email, presence:true
-
   def has_uid
     self.uid?
   end
 
-  def songs_attributes=(params)
-    params.each do |key, value|
-      if value["name"] == ""
-        puts "do nothing"
-      else
-        song = Song.create(value)
-        song.user = self
-        song.save
-      end
-    end
-  end
 end
