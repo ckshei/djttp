730e0f91a1e624aaf410e12e9a10a03c63e239f6
ckshei <cks856@gmail.com>
2016-10-11 16:12:19 -0400

    removed misc actions from the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,12 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: [:new, :spotify, :create, :login]
+
   def new
     if current_user
       redirect_to welcome_path
-    else
-      @user = User.new
-      @user.songs.build
-      @user.songs.build
-      @user.songs.build
     end
   end
+
   def spotify
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
     user = UserAdapter.create(spotify_user)
@@ -19,32 +16,11 @@ class UsersController < ApplicationController
     else
       flash[:error] = 'You fucked up'
     end
-
   end
+
   def home
     current_user
     @event = Event.new
-  end
-  def create
-    @user = User.create(user_params)
-    if @user.save 
-      UserAdapter.top_tracks(@user)
-      session[:user_id] = @user.id
-      redirect_to welcome_path
-    else
-      render :new 
-    end
-  end
-
-  def login
-    return redirect_to root_path, notice: "Cannot find username #{params[:user][:display_name]}" unless @user = User.find_by(display_name: params[:user][:display_name])
-    if @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to welcome_path
-    else
-      flash[:notice] = "Password is incorrect" 
-      redirect_to root_path
-    end
   end
 
   def logout
