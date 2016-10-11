90ccb38f1b85a4127f2bc47255965c9085df85bc
ckshei <cks856@gmail.com>
2016-10-11 16:15:14 -0400

    reset database

--- db/schema.rb
+++ db/schema.rb
@@ -39,13 +39,6 @@ ActiveRecord::Schema.define(version: 20160923194658) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "songs", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.integer  "uid",             limit: 8
     t.string   "display_name"
