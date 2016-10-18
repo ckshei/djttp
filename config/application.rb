74df717510248c509dba994b40069ee090c98e14
ckshei <cks856@gmail.com>
2016-10-17 20:27:53 -0400

    debugged dotenv var

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'rspotify'
 require 'set'
-RSpotify::authenticate(ENV['SPOTIFY_ID'], ENV['SPOTIFY_SECRET'])
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
