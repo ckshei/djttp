ba96a280446bc51fb298a73e0c91095770902d4f
ckshei <cks856@gmail.com>
2016-10-19 14:08:44 -0400

    adjusted keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['SPOTIFY_ID'], ENV['SPOTIFY_SECRET'], scope: 'user-top-read user-read-email playlist-modify-public user-read-birthdate user-read-private'
+  provider :spotify, "a8a0f594654c40aa93e0d1086567c57a", "b5128b5355f941338a9c6295aaf30aeb", scope: 'user-top-read user-read-email playlist-modify-public user-read-birthdate user-read-private'
 end
