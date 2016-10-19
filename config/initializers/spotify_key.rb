8735925b936bd0050875866e7f6638c26240c211
ckshei <cks856@gmail.com>
2016-10-19 14:01:56 -0400

    adjusted keys

--- config/initializers/spotify_key.rb
+++ config/initializers/spotify_key.rb
@@ -1,1 +1,1 @@
-RSpotify::authenticate(ENV['a8a0f594654c40aa93e0d1086567c57a'], ENV['b5128b5355f941338a9c6295aaf30aeb'])
+RSpotify::authenticate('a8a0f594654c40aa93e0d1086567c57a', 'b5128b5355f941338a9c6295aaf30aeb')
