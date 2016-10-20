b0efd7770db884e15341947f7446e6ae8b4ce513
ckshei <cks856@gmail.com>
2016-10-20 16:46:33 -0400

    testing moving rspotify auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :require_login, :current_user
+  RSpotify::authenticate(ENV['SPOTIFY_ID'], ENV['SPOTIFY_KEY'])
   private
 
   def require_login
