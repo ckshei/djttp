752a5725eee8ee5997e4880651ed60c0d89fb76b
ckshei <cks856@gmail.com>
2016-10-21 10:12:42 -0400

    added figaro

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['SPOTIFY_ID'], ENV['SPOTIFY_SECRET'], scope: 'user-top-read user-read-email playlist-modify-public user-read-birthdate user-read-private'
+  provider :spotify, ENV['SPOTIFY_ID'], ENV['SPOTIFY_KEY'], scope: 'user-top-read user-read-email playlist-modify-public user-read-birthdate user-read-private'
 end
