0c247a869b239392b786c590321664c2c126dc6f
ckshei <cks856@gmail.com>
2016-10-21 10:34:31 -0400

    adjusting keys

--- config/initializers/spotify_key.rb
+++ config/initializers/spotify_key.rb
@@ -1,4 +1,1 @@
-  puts ENV['SPOTIFY_ID']
-  puts ENV['SPOTIFY_KEY']
-  puts "hello"
-  RSpotify::authenticate(ENV['SPOTIFY_ID'], ENV['SPOTIFY_KEY'])
+  RSpotify::authenticate('a8a0f594654c40aa93e0d1086567c57a', 'b5128b5355f941338a9c6295aaf30aeb')
