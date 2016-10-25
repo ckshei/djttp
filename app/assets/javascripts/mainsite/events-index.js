c8347423a4ed7d36cb03a0657239a9f78a1a092c
ckshei <cks856@gmail.com>
2016-10-25 10:17:42 -0400

    adjusted sync-url to match for the show page

--- app/assets/javascripts/mainsite/events-index.js
+++ app/assets/javascripts/mainsite/events-index.js
@@ -3,19 +3,19 @@ App.eventsIndex = { run: function() {
 
     $.get("/events.json", function(data) {
       data.map(function(event_data) {
-        var ev = new Event(event_data.id, event_data.name, event_data.date, event_data.playlist.spotify_url, event_data.playlist.rsvp_url, event_data.guest_count, event_data.event_url, event_data.playlist.song_count)
+        var ev = new Event(event_data.id, event_data.name, event_data.date, event_data.playlist.spotify_url, event_data.playlist.sync_url, event_data.guest_count, event_data.event_url, event_data.playlist.song_count)
         var markup = ev.createDiv()
         $("#js-events-list").append(markup)
       });
     });
 
   class Event {
-    constructor(id, name, date, spotify_url, rsvp_url, guest_count, event_url, song_count) {
+    constructor(id, name, date, spotify_url, sync_url, guest_count, event_url, song_count) {
       this.id = id
       this.name = name
       this.date = date
       this.spotify_url = spotify_url
-      this.rsvp_url = rsvp_url
+      this.sync_url = sync_url
       this.guest_count = guest_count
       this.event_url = event_url
       this.song_count = song_count
@@ -29,13 +29,12 @@ App.eventsIndex = { run: function() {
           <div class="divider"></div>
           <div class="col-xs-12 event-details">
             <h3 class='event-name'><a href="/events/${this.id}">${this.name}</a></h3>
-            <p>Guest Count: ${this.guest_count} </p>
             <p>Song Count: ${this.song_count} </p>
             <a href="${this.spotify_url}" target="_blank">Spotify Playlist Link</a>
           </div>
           <div class="col-xs-12 rsvp">
             <h3>Sync Link</h3>
-            <p> ${this.rsvp_url} </p>
+            <p> ${this.sync_url} </p>
           </div>
       </div>
     </li>
--- app/assets/javascripts/mainsite/events-show.js
+++ app/assets/javascripts/mainsite/events-show.js
@@ -1,7 +1,7 @@
 App.eventsShow = {
   run: function() {
     $.get("/events/" + $(".js-event").data("id") + ".json", function(data) {
-        var ev = new Event(data.id, data.name, data.date, data.playlist.spotify_url, data.playlist.rsvp_url, data.guest_count, data.event_url, data.playlist.song_count)
+        var ev = new Event(data.id, data.name, data.date, data.playlist.spotify_url, data.playlist.sync_url, data.guest_count, data.event_url, data.playlist.song_count)
         var markup = ev.createDiv()
         $(".js-event").append(markup)
     $.get("/events/" + $(".js-event").data('id') + "/playlist.json", function(data) {
@@ -17,12 +17,12 @@ App.eventsShow = {
     });
 
   class Event {
-    constructor(id, name, date, spotify_url, rsvp_url, guest_count, event_url, song_count) {
+    constructor(id, name, date, spotify_url, sync_url, guest_count, event_url, song_count) {
       this.id = id
       this.name = name
       this.date = date
       this.spotify_url = spotify_url
-      this.rsvp_url = rsvp_url
+      this.sync_url = sync_url
       this.guest_count = guest_count
       this.event_url = event_url
       this.song_count = song_count
@@ -35,13 +35,12 @@ App.eventsShow = {
           <div class="divider"></div>
           <div class="col-xs-12 event-details">
             <h3 class='event-name'>${this.name}</h3>
-            <p>Guest Count: ${this.guest_count} </p>
             <p>Song Count: ${this.song_count} </p>
             <a href="${this.spotify_url}" target="_blank">Spotify Playlist Link</a>
           </div>
           <div class="col-xs-12 rsvp">
             <h3>Sync Link</h3>
-            <p> ${this.rsvp_url} </p>
+            <p> ${this.sync_url} </p>
           </div>
       </div>
         `
